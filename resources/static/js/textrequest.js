69659318dea1a9114b4655e4d120c5b92d05ee73
elquintela <jaelsonquintela@protonmail.com>
2020-12-29 17:51:03 -0300

    autenticacao falhando

--- resources/static/js/textrequest.js
+++ resources/static/js/textrequest.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const TextToSpeechV1 = require('ibm-watson/text-to-speech/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
+module.exports.lerComentarios = function(id) {
+
 const textToSpeech = new TextToSpeechV1({
   authenticator: new IamAuthenticator({ apikey: 'GBggpJhC6njXJCQ1WYHzX7x2K6--6_LizFI9nXDrtRAv' }),
-  serviceUrl: 'https://api.us-south.text-to-speech.watson.cloud.ibm.com'
+  serviceUrl: 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/e59448d3-b384-4ff0-8bbb-dfce101e8977'
 });
 
-module.exports.lerComentarios = function(id) {
 const params = {
-  text: 'Texto falado Brasil',
+  text: id,
   voice: 'pt-BR_IsabelaVoice', // Optional voice
   accept: 'audio/wav'
 };
--- server.js
+++ server.js
@@ -6,15 +6,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('resources'));
 
-app.use((req, res, next) => {
-	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
-	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
-
 global.__basedir = __dirname;
 
 const db = require('./app/config/db.config.js');
